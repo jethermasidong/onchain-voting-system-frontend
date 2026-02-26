26e82b2c1834220620be7e66d95e69e7585c6592
jethermasidong <jethermasidong05@gmail.com>
2026-02-26 20:49:43 +0800

    Progress: Initialized the project, started with the app/main page of the website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import background from './images/phflag.jpg';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 
-function App() {
+function LoginScreen() {
+
+  const navigate = useNavigate();
+  const handleLoginClick = () => {
+    navigate('/login');
+  };
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="min-h-screen relative flex items-center justify-center bg-gray-900">
+      
+      <div 
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat opacity-25"
+        style={{ backgroundImage: `url(${background})` }}
+      ></div>
+
+      <div className="relative z-10 bg-white w-full max-w-md p-8 rounded-lg shadow-xl">
+        
+        <div className="text-center mb-6">
+          <h1 className="text-2xl font-bold text-gray-800">BrgyChain Voting</h1>
+          <p className="text-sm text-gray-500">Secure. Transparent. Immutable.</p>
+        </div>
+
+        <div className="text-center p-4 border-2 border-solid border-gray-300 cursor-pointer rounded-lg">
+          <button onClick={handleLoginClick} className="text-black-400" >Click to Login!</button>
+        </div>
+
+      </div>
+      
     </div>
   );
 }
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<LoginScreen />} />
+        
+      </Routes>
+    </BrowserRouter>
+  );  
+}
 export default App;

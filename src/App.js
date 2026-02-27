d060f327824dad262d1481c05511e4b390b60011
jethermasidong <jethermasidong05@gmail.com>
2026-02-27 18:04:25 +0800

    Progress: Changed the structure of the app.js and make the login.js as pages instead

--- src/App.js
+++ src/App.js
@@ -1,53 +1,14 @@
 import React from 'react';
-import background from './images/phflag.jpg';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function LoginScreen() {
+import Login from './pages/login.js'; 
 
-  const navigate = useNavigate();
-  const handleLoginClick = () => {
-    navigate('/login');
-  };
-
-
-
-
-
-  return (
-
-    <div className="min-h-screen relative flex items-center justify-center bg-gray-900">
-      
-      <div 
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat opacity-25"
-        style={{ backgroundImage: `url(${background})` }}
-      ></div>
-
-      <div className="relative z-10 bg-white w-full max-w-md p-8 rounded-lg shadow-xl">
-        
-        <div className="text-center mb-6">
-          <h1 className="text-2xl font-bold text-gray-800">BrgyChain Voting</h1>
-          <p className="text-sm text-gray-500">Secure. Transparent. Immutable.</p>
-        </div>
-
-        <div className="text-center p-4 border-2 border-solid border-gray-300 cursor-pointer rounded-lg">
-          <button onClick={handleLoginClick} className="text-black-400" >Click to Login!</button>
-        </div>
-
-      </div>
-      
-    </div>
-  );
-}
-
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        <Route path="/" element={<LoginScreen />} />
-        
+        <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
-  );  
+  );
 }
-export default App;
---
+++ src/pages/login.js
@@ -0,0 +1,40 @@
+import background from "../images/phflag.jpg";
+import { useNavigate } from "react-router-dom";
+import React from 'react';
+
+
+
+
+export default function Login() {
+
+  const navigate = useNavigate();
+  const handleLoginClick = () => {
+    navigate('/login');
+  };
+
+  return (
+
+    <div className="min-h-screen relative flex items-center justify-center bg-gray-900">
+      
+      <div 
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat opacity-25"
+        style={{ backgroundImage: `url(${background})` }}
+      ></div>
+
+      <div className="relative z-10 bg-white w-full max-w-md p-8 rounded-lg shadow-xl">
+        
+        <div className="text-center mb-6">
+          <h1 className="text-2xl font-bold text-gray-800">BrgyChain Voting</h1>
+          <p className="text-sm text-gray-500">Secure. Transparent. Immutable.</p>
+        </div>
+
+        <div className="text-center p-4 border-2 border-solid border-gray-300 cursor-pointer rounded-lg">
+          <button onClick={handleLoginClick} className="text-black-400" >Click to Login!</button>
+        </div>
+
+      </div>
+      
+    </div>
+  );
+}
+

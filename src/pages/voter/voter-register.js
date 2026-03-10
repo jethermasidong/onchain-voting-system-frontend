579e289635881f124ce868d05c40857f66da15bb
jethermasidong <jethermasidong05@gmail.com>
2026-03-10 11:46:44 +0800

    Progress: Connect Voter Registration with Backend

--- src/pages/voter/voter-register.js
+++ src/pages/voter/voter-register.js
@@ -1,16 +1,47 @@
 import Logo from "../../images/logo.jpeg";
+import React, {useState} from 'react';
+import axios from 'axios';
+
 
 export default function VoterRegister() {
     
+  const [voters_id, setVotersId] = useState("");
+  const [name_hash, setNameHash] = useState("");
+  const [precinct_number, setPrecinctNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-    
-    
-    
-    
-    
-    
+  const [success, setSuccess] = useState(false);
 
+  const handleRegister = async () => {
+
+    try {
+      setSuccess(true);
+      setTimeout(() => setSuccess(false), 3000);
+
+      const token = localStorage.getItem('token');
+      await axios.post(
+        "http://localhost:3000/api/insert-voter",
+        {voters_id, name_hash, precinct_number, password, role: "voter"},
+        {headers: {Authorization: `Bearer ${token}`},
+      })
+
+      setVotersId("");
+      setNameHash("");
+      setPrecinctNumber("");
+      setPassword("");
+
+
+    } catch (error) {
+      console.error("Error Message:", error.message)
+    }
+  }
     
+    
+    
+    
+    
+ 
     
     return (
         <div class="max-w-4xl max-sm:max-w-lg mx-auto p-6 mt-36">
@@ -25,26 +56,46 @@ export default function VoterRegister() {
         <div class="grid sm:grid-cols-1 gap-8">
           <div>
             <label class="text-slate-900 text-sm font-medium mb-2 block">Voters ID</label>
-            <input name="voters_id" type="text" class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all" placeholder="Enter Voter ID" />
+            <input name="voters_id" type="text" 
+            value={voters_id}
+            onChange={(e) => setVotersId(e.target.value)}
+            class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all" placeholder="Enter Voter ID" />
           </div>
           <div>
             <label class="text-slate-900 text-sm font-medium mb-2 block">Full Name</label>
-            <input name="name_hash" type="text" class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all" placeholder="Enter full name" />
+            <input name="name_hash" type="text" 
+            value={name_hash}
+            onChange={(e) => setNameHash(e.target.value)}
+            class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all" placeholder="Enter full name" />
           </div>
           <div>
             <label class="text-slate-900 text-sm font-medium mb-2 block">Precinct Number</label>
-            <input name="precinct_number" type="text" class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all" placeholder="Enter precinct number" />
+            <input name="precinct_number" type="text" 
+            value={precinct_number}
+            onChange={(e) => setPrecinctNumber(e.target.value)}
+            class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all" placeholder="Enter precinct number" />
           </div>
           <div>
             <label class="text-slate-900 text-sm font-medium mb-2 block">Password</label>
-            <input name="password" type="text" class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all" placeholder="Enter password" />
+            <input name="password" 
+            type={showPassword ? "text" : "password"}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all" placeholder="Enter password" />
+            <button type="button"
+            onClick={() => setShowPassword((prev) => !prev)}>{showPassword ? "Hide" : "Show"}</button>
           </div>
         </div>
         <div class="mt-12">
-          <button type="button" class="mx-auto block min-w-32 py-3 px-6 text-sm font-medium tracking-wider rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none cursor-pointer">
+          <button type="button" onClick={handleRegister} class="mx-auto block min-w-32 py-3 px-6 text-sm font-medium tracking-wider rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none cursor-pointer">
             Register
           </button>
         </div>
+        { success && (
+          <div className="bg-green-600 border border-green-700 text-green-400 px-4 py-3 rounded relative mb-4">
+            Voter Registered Successfully!
+          </div>
+        )}
       </form>
     </div>
     )

8b16dbea3527e7ff3eeb3483b9cce9de9dc7a790
jethermasidong <jethermasidong05@gmail.com>
2026-03-12 11:14:45 +0800

    Progress: Fixed Login and Error Layout, Design

--- src/pages/login.js
+++ src/pages/login.js
@@ -18,9 +18,12 @@ export default function Login() {
         const newErrors = {};
         if (!voters_id) newErrors.voters_id = "Voters ID is required!";
         if (!password) newErrors.password = "Password is required!";
-        setErrors(newErrors)
-        if (Object.keys(newErrors).length > 0) return;
-
+        if (Object.keys(newErrors).length > 0) {
+          setErrors(newErrors);
+          if ("vibrate" in navigator) {
+            navigator.vibrate(200);
+          } return;
+        }
         try {
             const response = await axios.post(
                 "http://localhost:3000/api/login",
@@ -41,7 +44,7 @@ export default function Login() {
         } catch (error) {
             console.error("Full Error", error);
             if (error.response) {
-                setErrors({form: error.response.data.message || "Invalid credentials."});
+                setErrors({form: error.response.data.message || "Incorrect Voter ID or Password."});
             } else {
                 setErrors({form: "Cant connect to the server. Please try again later!"});
             }
@@ -53,10 +56,9 @@ export default function Login() {
 
     <div class="bg-white flex items-center justify-center md:h-screen p-4">
       <div class="[box-shadow:rgba(149,157,165,0.3)_0px_4px_18px] max-w-3xl max-md:max-w-lg rounded-lg p-7">
-        <a href="javascript:void(0)"><img
-          src={Logo} alt="logo" class="w-36 md:mb-4 mb-12" />
+        <a href="javascript:void(0)">
+          <img src={Logo} alt="logo" class="w-36 md:mb-4 mb-12" />
         </a>
-
         <div class="grid md:grid-cols-2 items-center gap-8">
           <div class="max-md:order-1">
             <div class="aspect-[12/11]">
@@ -64,14 +66,14 @@ export default function Login() {
             </div>
           </div>
 
-          <form class="md:max-w-md w-full mx-auto" onSubmit={handleLoginClick}>
-            <div class="mb-14">
-              <h1 class="text-4xl font-bold font-serif text-green-600">Sign in</h1>
-              {errors.form && <p className="text-red-500 text-sm mt-2">{errors.form}</p>}
+          <form className={`md:max-w-md w-full mx-auto ${errors.form ? 'animate-shake' : ''}`} onSubmit={handleLoginClick}>
+            <div class="mb-8">
+              <h1 class="text-4xl font-bold font-mono text-green-600">Sign in</h1>
             </div>
             <div>
+              <p class='text-sm text-gray-950 mb-1 font-mono'>Voter ID</p>
               <div class="relative flex items-center">
-                <input name="email" type="text" class="w-full text-sm border-b border-gray-300 focus:border-blue-600 pr-8 px-2 py-3 outline-none font-serif" placeholder="Enter your Voter ID" value={voters_id} onChange={(e) => setVotersId(e.target.value)} />
+                <input name="voter_id" type="text" required class="w-full text-sm border rounded-md border-blue-300 focus:border-blue-600 pr-8 px-2 py-3 outline-none font-mono mb-2" placeholder="Enter your Voter ID" value={voters_id} onChange={(e) => setVotersId(e.target.value)} />
                 <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-2" viewBox="0 0 682.667 682.667">
                   <defs>
                     <clipPath id="a" clipPathUnits="userSpaceOnUse">
@@ -84,24 +86,26 @@ export default function Login() {
                   </g>
                 </svg>
               </div>
-              {errors.voters_id && <p className="text-red-500 text-sm mt-2">{errors.voters_id}</p>}
             </div>
-            <div class="mt-4">
+            <div class="mt-1">
+              <p class='text-sm text-gray-950 mb-1 font-mono'>Password</p>
               <div class="relative flex items-center">
-                <input name="password" type="password" required class="w-full text-sm border-b border-gray-300 focus:border-blue-600 pr-8 px-2 py-3 outline-none font-serif" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input name="password" type="password" required class="w-full text-sm border rounded-md border-gray-300 focus:border-blue-600 pr-8 px-2 py-3 outline-none font-mono" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 <svg fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-7 cursor-pointer" viewBox="0 0 128 128">
                   <path d="M64 d104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
                 </svg>
               </div>
             </div>
-                <div class="flex flex-wrap items-center justify-between gap-4 mt-6">
-                {errors.password && <p className="text-red-500 text-sm mt-2">{errors.password}</p>}
+                <div class="flex flex-wrap items-center justify-between gap-4 mt-4">
             </div>
 
-            <div class="mt-5">
+            <div class="mt-2 mb-4">
               <button type="submit" class="w-full shadow-xl py-2 px-4 text-[15px] font-medium tracking-wide rounded-md cursor-pointer font-serif text-white bg-green-600 hover:bg-green-700 focus:outline-none">
                 Sign in
               </button>
+            </div>
+            <div className='min-h-[30px]'>
+            {errors.form && <p className="text-red-500 text-xs mt-1 border rounded-md border-red-300 px-2 py-1 text-center">{errors.form}</p>}
             </div>
           </form>
         </div>

366e549549834a1c164cf2e905188b28db97ab1d
jethermasidong <jethermasidong05@gmail.com>
2026-03-13 20:08:23 +0800

    Progress: Fixed Voter Register Layout

--- src/pages/voter/voter-register.js
+++ src/pages/voter/voter-register.js
@@ -44,8 +44,8 @@ export default function VoterRegister() {
  
     
     return (
-        <div class="max-w-4xl max-sm:max-w-lg mx-auto p-6 mt-36">
-      <div class="text-center mb-12 sm:mb-16">
+        <div class="max-w-4xl max-sm:max-w-lg mx-auto p-6 mt-34">
+      <div class="text-center mb-12 sm:mb-16 border border-black px-5 py-5 rounded-xl">
         <a href="javascript:void(0)"><img
           src={Logo} alt="logo" class='w-64 inline-block' />
         </a>
@@ -53,27 +53,27 @@ export default function VoterRegister() {
       </div>
 
       <form>
-        <div class="grid sm:grid-cols-1 gap-8">
+        <div class="bg-blue-100 grid sm:grid-cols-1 gap-6 border border-gray-700 px-4 py-4 rounded-xl shadow-xl">
           <div>
             <label class="text-slate-900 text-sm font-medium mb-2 block">Voters ID</label>
             <input name="voters_id" type="text" 
             value={voters_id}
             onChange={(e) => setVotersId(e.target.value)}
-            class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all" placeholder="Enter Voter ID" />
+            class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all border border-black" placeholder="Enter Voter ID" />
           </div>
           <div>
             <label class="text-slate-900 text-sm font-medium mb-2 block">Full Name</label>
             <input name="name_hash" type="text" 
             value={name_hash}
             onChange={(e) => setNameHash(e.target.value)}
-            class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all" placeholder="Enter full name" />
+            class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all border border-black" placeholder="Enter full name" />
           </div>
           <div>
             <label class="text-slate-900 text-sm font-medium mb-2 block">Precinct Number</label>
             <input name="precinct_number" type="text" 
             value={precinct_number}
             onChange={(e) => setPrecinctNumber(e.target.value)}
-            class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all" placeholder="Enter precinct number" />
+            class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all border border-black" placeholder="Enter precinct number" />
           </div>
           <div>
             <label class="text-slate-900 text-sm font-medium mb-2 block">Password</label>
@@ -81,13 +81,13 @@ export default function VoterRegister() {
             type={showPassword ? "text" : "password"}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all" placeholder="Enter password" />
+            class="bg-slate-100 w-full text-slate-900 text-sm px-4 py-3 rounded-md focus:bg-transparent outline-green-500 transition-all border border-black" placeholder="Enter password" />
             <button type="button"
             onClick={() => setShowPassword((prev) => !prev)}>{showPassword ? "Hide" : "Show"}</button>
           </div>
         </div>
         <div class="mt-12">
-          <button type="button" onClick={handleRegister} class="mx-auto block min-w-32 py-3 px-6 text-sm font-medium tracking-wider rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none cursor-pointer">
+          <button type="button" onClick={handleRegister} class="mx-auto block min-w-32 py-3 px-6 text-sm font-medium tracking-wider rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none cursor-pointer border border-black">
             Register
           </button>
         </div>
